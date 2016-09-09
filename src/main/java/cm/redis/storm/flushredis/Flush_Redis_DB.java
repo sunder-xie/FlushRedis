b4ae6d7ff7811ec8fd9858c9d36cd0735d4893fb
chinamobile <yanxuexu007@126.com>
2016-09-09 15:19:46 +0800

    数据更新设计完成，尚缺维表数据更新。

--- src/main/java/cm/redis/storm/flushredis/Flush_Redis_DB.java
+++ src/main/java/cm/redis/storm/flushredis/Flush_Redis_DB.java
@@ -207,7 +207,7 @@ public class Flush_Redis_DB {
 					Flush_Redis_DB.flush_g4jk();
 				    if(TimeFormatter.getHour().equals("03")==true){
 						// 每天凌晨 3 点检查维表更新，更新添加维表信息，如果获取不到最新数据，维表信息在redis中可能为空
-						// Flush_Redis_DB.flush_g4jk_ref();
+						Flush_Redis_DB.flush_g4jk_ref();
 						// 获取接口数据，更新ref维表信息，所有数据文件第一行为列名，用;隔开，第二行开始是数据记录，记录内数据之间同样用分号隔开
 						Flush_Redis_DB.update_g4jk_ref(null,"custtag");	//"d243c012-5ef5-4537-ad75-21c4b90fe74f"
 						Flush_Redis_DB.update_g4jk_ref(null,"hotspot");	//"c1ed7776-a16b-4472-a1bd-954df3925466"
