db7eafd15cfb0730b3e1693aa9683d649c3434d9
chinamobile <yanxuexu007@126.com>
2016-09-12 22:29:12 +0800

    666

--- src/main/java/cm/redis/commons/ResourcesConfig.java
+++ src/main/java/cm/redis/commons/ResourcesConfig.java
@@ -21,10 +21,8 @@ public class ResourcesConfig {
 	public final static int CLUSTER_TIMEOUT=60*1000;						//获取集群信息的超时时间
 	public final static int CLUSTER_MAX_REDIRECTIONS=15; 			//获取重定向的次数
 	
-	public final static String REDIS_SERVER_IP1="10.245.254.56";		//redis数据库IP地址
-	public final static String REDIS_SERVER_IP2="10.245.254.30";		//redis数据库IP地址
-	public final static String REDIS_SERVER_IP3="10.245.254.31";		//redis数据库IP地址
-	public final static String REDIS_SERVER_PORT="7000";				//redis数据库端口
+	public final static String REDIS_SERVER_IP="10.245.254.56";		//redis数据库IP地址
+	public final static String REDIS_SERVER_PORT="7001";				//redis数据库端口
 	
 	// 数据枢纽接口的文件，以及数据存放路径，最后带有/
 	public final static String SYN_SERVER_DATAFILE="/home/storm/netantdata/ref/";  //正式服务器地址
