f6f1766fbeebf7306beb05e36ff400f1d4c58ae1
chinamobile <yanxuexu007@126.com>
2016-09-29 22:59:44 +0800

    修改单例模式

--- src/main/java/cm/redis/commons/RedisServer.java
+++ src/main/java/cm/redis/commons/RedisServer.java
@@ -67,6 +67,13 @@ public class RedisServer {
 	 * @return
 	 */
 	public static RedisServer getInstance() {
+		if(INSTANCE==null){
+			synchronized (RedisServer.class) {
+				if(INSTANCE==null){
+					INSTANCE=new RedisServer();
+				}
+			}
+		}
 		return INSTANCE;
 	}
 	
