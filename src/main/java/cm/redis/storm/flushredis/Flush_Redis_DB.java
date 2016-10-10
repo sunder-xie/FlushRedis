048a7b12588e6b864488581d54e76329b550e3b3
chinamobile <yanxuexu007@126.com>
2016-10-10 09:51:05 +0800

    666

--- src/main/java/cm/redis/storm/flushredis/Flush_Redis_DB.java
+++ src/main/java/cm/redis/storm/flushredis/Flush_Redis_DB.java
@@ -47,7 +47,7 @@ public class Flush_Redis_DB {
 					Flush_Redis_DB.flush_biglogs();
 					// 每天凌晨 3 点与下午14点执行，负责清理网分数据过期的实时信息
 					Flush_Redis_DB.flush_g4jk();
-				    if(TimeFormatter.getHour().equals("14")==true){ //每天更新一次维表信息
+				    if(TimeFormatter.getHour().equals("03")==true){ //每天更新一次维表信息
 						// 每天凌晨 3 点检查维表更新，更新添加维表信息，如果获取不到最新数据，维表信息在redis中可能为空
 						Flush_Redis_DB.flush_g4jk_ref();
 						// 获取接口数据，更新ref维表信息，所有数据文件第一行为列名，用;隔开，第二行开始是数据记录，记录内数据之间同样用分号隔开
