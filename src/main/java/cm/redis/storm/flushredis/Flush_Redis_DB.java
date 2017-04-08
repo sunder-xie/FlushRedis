c1385503df8e89bdb9dace089b89f5773f9b4acc
chinamobile <yanxuexu007@126.com>
2017-04-08 20:04:07 +0800

    666

--- src/main/java/cm/redis/storm/flushredis/Flush_Redis_DB.java
+++ src/main/java/cm/redis/storm/flushredis/Flush_Redis_DB.java
@@ -34,52 +34,52 @@ public class Flush_Redis_DB {
 	public static void main(String[] args)
 	{
 		//测试代码段
-		Flush_Redis_DB.flush_g4jk();
+//		Flush_Redis_DB.flush_g4jk();
 //		Flush_Redis_DB.flush_g4jk_ref();
 		
 		//正式代码段
-//		boolean cleanonce=false;
-//		while(true)
-//		{
-//			//每天固定凌晨3点清理一次数据
-//			if(TimeFormatter.getHour().equals("03")==true||TimeFormatter.getHour().equals("15")==true)
-//			{
-//				if(cleanonce==false){
-//					// 每天凌晨 3 点与下午14点执行，负责清理大日志数据过期的实时信息，暂停
-//					//Flush_Redis_DB.flush_biglogs();
-//					 if(TimeFormatter.getHour().equals("03")==true){ //每天更新一次维表信息
-//						//每天凌晨 3 点与下午14点执行，负责清理网分数据过期的实时信息
-//						Flush_Redis_DB.flush_g4jk();
-//						// 每天凌晨 3 点检查维表更新，更新添加维表信息，如果获取不到最新数据，维表信息在redis中可能为空
-//						Flush_Redis_DB.flush_g4jk_ref();//仅清理掉昨天过期的触点参数信息数据
-//						// 获取接口数据，更新ref维表信息，所有数据文件第一行为列名，用;隔开，第二行开始是数据记录，记录内数据之间同样用分号隔开
-//						Flush_Redis_DB.update_g4jk_ref(null, "tcsll");		//直接对已有的ref文件进行更新，这个维表不会经常更新，ref文件:tb_mofang_tcsll_ref.txt
-//						Flush_Redis_DB.update_g4jk_ref(null, "webtag"); //直接对已有的ref文件进行更新，这个维表不会经常更新，ref文件:tb_mofang_webtag_ref.txt
-//						//Flush_Redis_DB.update_g4jk_ref("d243c012-5ef5-4537-ad75-21c4b90fe74f","custtag");			//用户标签维表，仅双11使用，标记用户的对应人群属性，每天更新一次，d243c012-5ef5-4537-ad75-21c4b90fe74f
-//						Flush_Redis_DB.update_g4jk_ref("c1ed7776-a16b-4472-a1bd-954df3925466", "hotspot");			//tac ci与热点区域转换维表，这个维表不会经常更新，c1ed7776-a16b-4472-a1bd-954df3925466
-//						Flush_Redis_DB.update_g4jk_ref("0b67bada-c954-418d-aa25-347b5810c679", "imsiphnum");  	//号码与imsi转换表，每天更新一次，0b67bada-c954-418d-aa25-347b5810c679
-//						//新需求：高流量使用，低余额，已经开发好的接口：26c068d5-5cf5-4951-9df4-0e597c4f0bbb，"amtflux"
-//				    }else if(TimeFormatter.getHour().equals("15")==true){
-//				    	//每天凌晨 3 点与下午15点执行，负责清理网分数据过期的实时信息
-//						Flush_Redis_DB.flush_g4jk();
-//				    }
-//				    cleanonce=true;
-//				}
-//			}
-//			else	{
-//				cleanonce=false;
-//			}
-//			
-//			//每隔1小时读取最新的接口触点关键字配置信息，用于提供给storm实时分析的指标
-//			Flush_Redis_DB.setSjjsParamsToRedis();
-//			RedisServer.close();
-//			
-//			try{
-//				Thread.sleep(1000*60*60);//休息1小时
-//			}catch(Exception e){
-//				logger.info(" Thread Flush_Redis_DB crashes: "+e.getMessage());
-//			}
-//		}
+		boolean cleanonce=false;
+		while(true)
+		{
+			//每天固定凌晨3点清理一次数据
+			if(TimeFormatter.getHour().equals("03")==true||TimeFormatter.getHour().equals("14")==true)
+			{
+				if(cleanonce==false){
+					// 每天凌晨 3 点与下午14点执行，负责清理大日志数据过期的实时信息，暂停
+					//Flush_Redis_DB.flush_biglogs();
+					 if(TimeFormatter.getHour().equals("03")==true){ //每天更新一次维表信息
+						//每天凌晨 3 点与下午14点执行，负责清理网分数据过期的实时信息
+						Flush_Redis_DB.flush_g4jk();
+						// 每天凌晨 3 点检查维表更新，更新添加维表信息，如果获取不到最新数据，维表信息在redis中可能为空
+						Flush_Redis_DB.flush_g4jk_ref();//仅清理掉昨天过期的触点参数信息数据
+						// 获取接口数据，更新ref维表信息，所有数据文件第一行为列名，用;隔开，第二行开始是数据记录，记录内数据之间同样用分号隔开
+						Flush_Redis_DB.update_g4jk_ref(null, "tcsll");		//直接对已有的ref文件进行更新，这个维表不会经常更新，ref文件:tb_mofang_tcsll_ref.txt
+						Flush_Redis_DB.update_g4jk_ref(null, "webtag"); //直接对已有的ref文件进行更新，这个维表不会经常更新，ref文件:tb_mofang_webtag_ref.txt
+						//Flush_Redis_DB.update_g4jk_ref("d243c012-5ef5-4537-ad75-21c4b90fe74f","custtag");			//用户标签维表，仅双11使用，标记用户的对应人群属性，每天更新一次，d243c012-5ef5-4537-ad75-21c4b90fe74f
+						Flush_Redis_DB.update_g4jk_ref("c1ed7776-a16b-4472-a1bd-954df3925466", "hotspot");			//tac ci与热点区域转换维表，这个维表不会经常更新，c1ed7776-a16b-4472-a1bd-954df3925466
+						Flush_Redis_DB.update_g4jk_ref("0b67bada-c954-418d-aa25-347b5810c679", "imsiphnum");  	//号码与imsi转换表，每天更新一次，0b67bada-c954-418d-aa25-347b5810c679
+						//新需求：高流量使用，低余额，已经开发好的接口：26c068d5-5cf5-4951-9df4-0e597c4f0bbb，"amtflux"
+				    }else if(TimeFormatter.getHour().equals("14")==true){
+				    	//每天凌晨 3 点与下午15点执行，负责清理网分数据过期的实时信息
+						Flush_Redis_DB.flush_g4jk();
+				    }
+				    cleanonce=true;
+				}
+			}
+			else	{
+				cleanonce=false;
+			}
+			
+			//每隔1小时读取最新的接口触点关键字配置信息，用于提供给storm实时分析的指标
+			Flush_Redis_DB.setSjjsParamsToRedis();
+			RedisServer.close();
+			
+			try{
+				Thread.sleep(1000*60*60);//休息1小时
+			}catch(Exception e){
+				logger.info(" Thread Flush_Redis_DB crashes: "+e.getMessage());
+			}
+		}
 	}
 	
 	/**
